e31e4ecda994364d62afa21ad9ffc25e61b45714
victorchafbe-hash <victor.chafbe@escola.pr.gov.br>
2026-07-31 11:29:10 -0300

    .

---
+++ media-foEarch.js
@@ -0,0 +1,11 @@
+const notas={10, 6.5,8,7.5};
+
+let soma das notas = 0;
+
+
+noats.forEach(funcition () {
+    console.log('oi');
+});
+const media = somadasnotas / notas.length;
+
+console.log('A media das notas é $(media).')
